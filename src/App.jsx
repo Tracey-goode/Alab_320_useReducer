112496594452f980c46074bc1a505a98b20b2ac2
Tracey-goode <Jerand322@gmail.com>
2025-10-05 22:01:57 -0400

    updating app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer, useState } from 'react'
+import { todoReducer } from './components/todoreducer'
+import initialState from './data/initialState'
+import './App.css';
 
 function App() {
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const [newTodo, setNewTodo] = useState("");
 
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (!newTodo.trim()) return;
+    dispatch({ type: "Add_todo", title: newTodo });
+    setNewTodo("");
+  };
 
   return (
-    <>
-     
-    </>
+    <div className="container">
+      <h1 className="title"> Todo List </h1>
+      <form onSubmit={handleAdd} className="todo-form">
+        <input
+          type="Text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)} placeholder="Add New!" className="input" />
+        <button>
+        
+        </button>
+      </form>
+    </div>
   )
 }
 
