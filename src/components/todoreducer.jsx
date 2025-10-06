727d22a203b3ecb35fec535696a1119ab48db057
Tracey-goode <Jerand322@gmail.com>
2025-10-06 00:59:16 -0400

    finished

--- src/components/todoreducer.jsx
+++ src/components/todoreducer.jsx
@@ -1,4 +1,4 @@
-export function todoReducer(state, action){
+export function todoReducer(state, action) {
     switch (action.type) {
         case "Add_todo":
             return [
@@ -9,15 +9,17 @@ export function todoReducer(state, action){
                 },
                 ...state,
             ];
-        case "Toggle_todo":
-            return state.map((todo) => 
-                todo.id === action.id ? { ...todo, completed: !todo.completed} : todo
+        case "Toggle":
+            return state.map((todo) =>
+                todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
             );
         case "Edit_todo":
-            return state.map((todo) => 
-            todo.id === action.id ? { ...todo, title: action.title } : todo 
+            return state.map((todo) =>
+                todo.id === action.id ? { ...todo, title: action.title } : todo
             );
-        case "Delete_todo"
+        case "DeleteTodo":
+            return state.filter((todo) => todo.id !== action.id);
+        default:
+            return state;
     }
-    return state;
 }
